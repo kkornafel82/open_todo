adb3b52b5751b82238cffd10a83eb4d0f3e2a9d7
Kevin <kkornafel82@yahoo.com>
2015-11-16 20:06:56 -0800

    create from the command line complete

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -11,4 +11,19 @@ class Api::UsersController < ApiController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+   def user_params
+     params.require(:user).permit(:email, :password)
+   end
+
  end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,15 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
 
   namespace :api, defaults: { format: :json } do
-     resources :users
+     resources :users do
+       resources :lists
+     end
+
+     resources :lists, only: [] do
+      resources :items, only: [:create]
+     end
+
+     resources :items, only: [:destroy]
   end
 
  
